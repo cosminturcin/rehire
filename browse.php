6633c5b20bbb7526717f45c882ae21752b2c3fb8
turcin cosmin <bbecosmin13gmail.com>
2016-10-11 19:31:06 +0300

    browse page

--- browse.php
+++ browse.php
@@ -3,8 +3,95 @@
     <?php include 'includes/header.php' ; ?>
 
     <!-- Page Content -->
-    <section class="browse-first">
-        <div class="container">sdfsddsff</div>
+    <section class="browse-first backgroundblue">
+        <div class="c1 container">
+        <h1>Advanced Search</h1>
+        <form>   
+         <select id="" name="">
+          <option value="">Select Category</option>
+          <option value=""></option>
+          <option value=""></option>
+         </select>
+         <select id="" name="">
+          <option value="">Select Topic</option>
+          <option value=""></option>
+          <option value=""></option>
+         </select>
+         <input type="text" name="search" placeholder="Enter keywords...">
+         <input type="submit" value="FIND EXPERTS">
+        </form>
+        </div>
+    </section>
+    <section class="backgroundgrey">
+        <div class="c2 container backgroundwhite">
+           <div class="col-sm100">
+              <div class="col-sm50">
+                 <div class="col-sm100">
+                     <div class="col-sm20">
+                       <div class="whiterect">
+                        <p>Showing:</p> 
+                       </div>  
+                     </div>
+                     <div class="col-sm20">
+                        <div class="bluerect">
+                          <p>FEATURED</p>
+                        </div> 
+                     </div>
+                     <div class="col-sm20">
+                         <div class="bluerect">
+                          <p>POPULAR</p>
+                         </div>
+                     </div>
+                     <div class="col-sm20">
+                          <div class="bluerect">
+                           <p>NEW</p>
+                          </div>
+                     </div>
+                     <div class="col-sm20">
+                         <div class="bluerect">
+                          <p>ALL</p>
+                         </div> 
+                     </div>
+                 </div> 
+              </div>
+              <div class="col-sm50">
+                <div class="col-sm100">
+                    <div class="col-sm-9">
+                        <p>Sort by:</p>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="bluerect2">
+                            <p>LOWEST PRICE <img src="img/shape.png" alt=""> </p>
+                        </div>
+                    </div>
+                </div>  
+              </div>
+           </div>
+        </div>
+        <div class="c3 container backgroundwhite">
+            <div class="col-sm100">
+                <div class="col-sm-3">
+                    <img class="img1" src="img/pic1.png" alt="">
+                </div>
+                <div class="col-sm-6">
+                    <h1>How to increase user visits</h1>
+                    <p><span>John Doe</span> - Sidney, Australia</p>
+                    <p>Pellentesque dapibus dui id purus malesuada, eu tempor elit eleifend. Cras tincidunt lacinia lectus, vitae iaculis est commodo eget. 
+                    Integer interdum vel turpis vitae porttitor. Mauris ut molestie diam. Nam at dui nunc...</p>
+                </div>
+                <div class="col-sm-3">
+                    <div class="col-sm100">
+                        <div class="col-sm-3">
+                            <div class="blackrect"><p>4</p></div>
+                        </div>
+                        <div class="col-sm-9">
+                            <img src="img/4stars.png" alt="">
+                        </div>
+                    </div>
+                    <p><span>£62</span>/hour</p>
+                </div>
+            </div>
+        </div>
     </section>
     <!-- /.container -->
 
